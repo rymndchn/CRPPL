1abe82a89419f43fa906003b496e5ca4ba140f2e
gvparcon09 <95574791+gvparcon09@users.noreply.github.com>
2022-02-14 00:18:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from antlr4 import *
 from CRPPLLexer import CRPPLLexer
 from CRPPLParser import CRPPLParser
 from myCRPPLListener import myCRPPLListener
-from antlr4.tree.Tree import TerminalNodeImpl
 
 import pandas as pd
-
-token = CRPPLParser.symbolicNames
 
 def main (argv):
     input = FileStream(argv[1])
@@ -26,7 +23,6 @@ def main (argv):
     myCRPPL = myCRPPLListener(output)
     walker = ParseTreeWalker()
     walker.walk(myCRPPL, tree)
-    #traverse(tree, parser.ruleNames)
         
     output.close()      
 
@@ -39,20 +35,6 @@ def main (argv):
     exec(program_store)
 
     print(df)
-
-def traverse(tree, rule_names, indent = 0):
-    if tree.getText() == "<EOF>":
-        return
-    elif isinstance(tree, TerminalNodeImpl):
-        #print("{0}TOKEN='{1}'".format("  " * indent, token[tree.getSymbol().type]))
-        #print("{0}LEXEME='{1}'".format("  " * indent, tree.getText()))
-        #print("{",token[tree.getSymbol().type],", \"",tree.getText(),"\"}")
-        print("   " * indent, "+--", token[tree.getSymbol().type], "-", tree.getText())
-    else:
-        #print("{0}{1}".format("  " * indent, rule_names[tree.getRuleIndex()]))
-        print("   " * indent, "+--", "RULE NAME: ", rule_names[tree.getRuleIndex()])
-        for child in tree.children:
-            traverse(child, rule_names, indent + 1)
 
 if __name__ == '__main__':
     main(sys.argv)
