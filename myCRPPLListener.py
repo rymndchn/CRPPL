a2a7bf4c83548c56ad1ea8af8509ce6221626732
James Kevin Lin <lin.james0825@yahoo.com>
2022-02-20 14:06:02 +0800

    fix tabbing in functions and generalquery

--- myCRPPLListener.py
+++ myCRPPLListener.py
@@ -40,14 +40,10 @@ class myCRPPLListener(CRPPLListener) :
                 self.else_ctr[self.if_nest_ctr]-=1
 
     def enterGeneralquery(self, ctx:CRPPLParser.GraphqueryContext):
-
-        if(self.tab_count > 0):
-            self.output.write('\t')
-            self.tab_count -= 1
-        else:
-            pass
-
+        
         if ctx.GET() is not None:
+            for i in range(0,self.if_nest_ctr):
+                self.output.write("\t")
             # get position of GET
             get_pos = int(re.search('(\[@)(\d+)(,.*)',str(ctx.GET().getSymbol())).group(2))
 
@@ -110,6 +106,8 @@ class myCRPPLListener(CRPPLListener) :
                             tmp_col = tmp_col + '"' + x + '",'
 
                         # assemble the command to do simple query
+                        for i in range(0,self.if_nest_ctr):
+                            self.output.write("\t")
                         command = 'print(' + tbl + '[[' + tmp_col[0:-1] + ']])'
                         self.output.write(command + '\n')
                     else: 
@@ -167,9 +165,16 @@ class myCRPPLListener(CRPPLListener) :
                         for x in cols:
                             tmp_col = tmp_col + '"' + x + '",'
 
-                        command = 'tmp_result = ' + tbl + '[' + tbl + '["' + tmp_cond1 + '"]' + tmp_cond2 + ']' + '\nprint(tmp_result' + '[[' + tmp_col[0:-1] + ']])'
+                        #command = 'tmp_result = ' + tbl + '[' + tbl + '["' + tmp_cond1 + '"]' + tmp_cond2 + ']' + '\nprint(tmp_result' + '[[' + tmp_col[0:-1] + ']])'
+                        command = 'tmp_result = ' + tbl + '[' + tbl + '["' + tmp_cond1 + '"]' + tmp_cond2 + ']' 
+                        command2 ='print(tmp_result' + '[[' + tmp_col[0:-1] + ']])'
 
                         self.output.write(command + '\n')
+
+                        for i in range(0,self.if_nest_ctr):
+                            self.output.write("\t")
+
+                        self.output.write(command2 + '\n')
 
                     if cond_count > 1: # more than 1 condition
 
@@ -224,9 +229,14 @@ class myCRPPLListener(CRPPLListener) :
                         for x in cols:
                             tmp_col = tmp_col + '"' + x + '",'
 
-                        command = 'tmp_result = ' + tbl + '[' + filter_statement + ']' + '\nprint(tmp_result' + '[[' + tmp_col[0:-1] + ']])'
+                        command = 'tmp_result = ' + tbl + '[' + filter_statement + ']' + '\n'
 
                         self.output.write(command + '\n')
+                        command2='print(tmp_result' + '[[' + tmp_col[0:-1] + ']])'
+                        
+                        for i in range(0,self.if_nest_ctr):
+                            self.output.write("\t")
+                        self.output.write(command2 + '\n')
 
                 else:
                     print('Coming soon!')
@@ -240,11 +250,9 @@ class myCRPPLListener(CRPPLListener) :
 
     # Enter a parse tree produced by CRPPLParser#defineconstant.
     def enterDefineconstant(self, ctx:CRPPLParser.DefineconstantContext):
-        if(self.tab_count > 0):
-            self.output.write('\t')
-            self.tab_count -= 1
-        else:
-            pass
+        self.output.write("#is constant\n")
+        for i in range(0,self.if_nest_ctr):
+            self.output.write("\t")
 
         if ctx.RESERVEDWORD_CONSTANT() is not None:
 
@@ -299,11 +307,8 @@ class myCRPPLListener(CRPPLListener) :
 
     def enterAssignment(self, ctx:CRPPLParser.AssignmentContext):
         
-        if(self.tab_count > 0):
-            self.output.write('\t')
-            self.tab_count -= 1
-        else:
-            pass
+        for i in range(0,self.if_nest_ctr):
+            self.output.write("\t")
 
         if ctx.ASSIGNEMT_OPERATOR() is not None:
 
@@ -320,7 +325,6 @@ class myCRPPLListener(CRPPLListener) :
         self.output.write('\n')
 
     def enterChangevalue(self, ctx:CRPPLParser.ChangevalueContext):
-        
         if(self.tab_count > 0):
             self.output.write('\t')
             self.tab_count -= 1
@@ -406,7 +410,8 @@ class myCRPPLListener(CRPPLListener) :
             if((end_pos-create_pos) == 1):
                 self.output.write('pass\n')
 
-            self.tab_count = len(ctx.generalquery()) + len(ctx.importfile()) + len(ctx.altercolumn()) + len(ctx.changevalue()) + len(ctx.expr()) + len(ctx.assignment()) + len(ctx.defineconstant()) + len(ctx.functioncall()) + len(ctx.graphquery()) + len(ctx.conditionalstatement())
+            #self.tab_count = len(ctx.generalquery()) + len(ctx.importfile()) + len(ctx.altercolumn()) + len(ctx.changevalue()) + len(ctx.expr()) + len(ctx.assignment()) + len(ctx.defineconstant()) + len(ctx.functioncall()) + len(ctx.graphquery()) + len(ctx.conditionalstatement())
+            self.if_nest_ctr+=1
         else:
             print('Error!')
 
@@ -427,7 +432,7 @@ class myCRPPLListener(CRPPLListener) :
 
         #end the function.
         if ctx.ENDFUNCTION() is not None:
-            self.output.write('\n')
+            self.if_nest_ctr-=1
         else:
             pass
 
@@ -533,11 +538,8 @@ class myCRPPLListener(CRPPLListener) :
                 self.inside_if=False
     def enterFunctioncall(self, ctx:CRPPLParser.FunctioncallContext):
         
-        if(self.tab_count > 0):
-            self.output.write('\t')
-            self.tab_count -= 1
-        else:
-            pass
+        for i in range(0,self.if_nest_ctr):
+            self.output.write("\t")
 
         if(self.tab_count > 0):
             self.output.write('\t')
