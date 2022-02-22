b77f6690c1fce2aa42c050887b73d74193755127
gvparcon09 <95574791+gvparcon09@users.noreply.github.com>
2022-02-22 17:36:24 +0800

    useCase3 initial

--- output.py
+++ output.py
@@ -3,36 +3,23 @@ import matplotlib.pyplot as plt
 import shutil
 import pandas as pd
 
-df["additional_col1"] = np.nan
-df.to_csv('CRPPL/CSV Files/target/df.csv', index=False)
-df["additional_col2"] = np.nan
-df.to_csv('CRPPL/CSV Files/target/df.csv', index=False)
-df.drop('additional_col2',axis=1, inplace=True)
-df.to_csv('CRPPL/CSV Files/target/df.csv', index=False)
-df.loc[df.Name=='Tom',"additional_col1"]='Thomas'
-def do_graph_in(df):
-	tmp_result = df[df["Age"]<20]
-	print(tmp_result[["Name","Age"]])
-	num = 3+1
-	tmp_result = df[df["Age"]<15]
-	print(tmp_result[["Name","Age"]])
+varFileName = 'CRPPL/CSV Files/source/1000 - Sales Records.csv'
+# i am checking line
+varDataSetName = 'CRPPL/CSV Files/target/sales.csv'
+# i am checking line
+source = r'CRPPL/CSV Files/source/1000 - Sales Records.csv'
+target = r'CRPPL/CSV Files/target/sales.csv'
+shutil.copyfile(source,target)
+sales = pd.read_csv('CRPPL/CSV Files/target/sales.csv')
 
-def do_graph_inside(revenue):
-	graph_name = 'x'
-	tmp_result = df[df["Age"]<20]
-	print(tmp_result[["Name","Age"]])
-	return graph_name
-
-counter = 0
-count_max = 3
-def add_num(counter, count_max):
-	if ( counter < count_max ) :
-				tmp_result = df[df["Age"]<20]
-		print(tmp_result[["Name","Age"]])
-	else:
-				counter = counter+1
-				add_num(counter, count_max)
-
-#end if
-
-do_graph_in(df)
+# i am checking line
+tmp_result = sales[[]]
+grouped = sales
+print(grouped)
+# i am checking line
+#inside grouping isTrue
+print(sales.plot.bar())
+plt.xlabel('rating')
+plt.ylabel('gross')
+plt.savefig('Report/bar.pdf',bbox_inches='tight')
+# i am checking line
