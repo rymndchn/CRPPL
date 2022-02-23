20b3e17c8c4b9f8371bb0525326120cfbae6e28e
gvparcon09 <95574791+gvparcon09@users.noreply.github.com>
2022-02-23 10:49:39 +0800

    update main

--- output.py
+++ output.py
@@ -3,48 +3,26 @@ import matplotlib.pyplot as plt
 import shutil
 import pandas as pd
 
-varFileName = 'CRPPL/CSV Files/source/1000 - Sales Records.csv'
-varDataSetName = 'CRPPL/CSV Files/target/sales.csv'
-source = r'CRPPL/CSV Files/source/1000 - Sales Records.csv'
-target = r'CRPPL/CSV Files/target/sales.csv'
+varFileName = 'CRPPL/CSV Files/source/1000 - Movie Reviews.csv'
+varDataSetName = 'CRPPL/CSV Files/target/movies.csv'
+source = r'CRPPL/CSV Files/source/1000 - Movie Reviews.csv'
+target = r'CRPPL/CSV Files/target/movies.csv'
 shutil.copyfile(source,target)
-sales = pd.read_csv('CRPPL/CSV Files/target/sales.csv')
+movies = pd.read_csv('CRPPL/CSV Files/target/movies.csv')
 
 #entering assignment
-data_count = 3
-#exiting assignment
-#entering assignment
-counter = 0
-#exiting assignment
-def get_country(counter, data_count, sales):
-	if ( counter == 0 ) :
-		tmp_result = sales[sales["DataId"]==0]
-		tmp_result = tmp_result[["Country"]]
-		print(tmp_result)
-	elif ( counter == 1 ) :
-		tmp_result = sales[sales["DataId"]==1]
-		tmp_result = tmp_result[["Country"]]
-		print(tmp_result)
-	elif ( counter == 2 ) :
-		tmp_result = sales[sales["DataId"]==2]
-		tmp_result = tmp_result[["Country"]]
-		print(tmp_result)
-	elif ( counter == 3 ) :
-		tmp_result = sales[sales["DataId"]==3]
-		tmp_result = tmp_result[["Country"]]
-		print(tmp_result)
-	elif ( counter == 4 ) :
-		tmp_result = sales[sales["DataId"]==4]
-		tmp_result = tmp_result[["Country"]]
-		print(tmp_result)
+tmp_result = movies[movies["rating"]!='X']
+grouped = tmp_result.groupby(["rating"])
+__________GROUPED_IS = True
+tmp_result = grouped.agg({"gross":"sum"})
+print(tmp_result)
+notXResult=grouped.agg({"gross":"sum"})
 
-#end if
-	if ( counter < data_count ) :
-#entering assignment
-		counter = counter+1
 #exiting assignment
-		get_country(counter, data_count, sales)
-
-#end if
-
-get_country(counter, data_count, sales)
+#inside grouping isFalse
+#insissdfdfdsFalse
+print(notXResult.plot.bar())
+plt.ticklabel_format(axis='y', style='plain')
+plt.xlabel('rating')
+plt.ylabel('gross')
+plt.savefig('Report/bar.pdf',bbox_inches='tight')
